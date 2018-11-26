b7f5f2215ec594686388b6a3f6de1db4c1113a8a
pawn840 <45342374+pawn840@users.noreply.github.com>
2018-11-26 13:49:30 +0900

    Update sparql-queries.js

--- docs/trial2/sparql-queries.js
+++ docs/trial2/sparql-queries.js
@@ -26,11 +26,13 @@ function getSPARQL002()
     var sparql_query = `
 PREFIX l4a-fin: <http://lod4all.net/ontology/financial/>
 
-SELECT ?date (xsd:decimal(?org_value) as ?value) WHERE {
-    <%URI%> l4a-fin:data ?financial_data.
-    ?financial_data l4a-fin:date ?org_date .
-    ?financial_data l4a-fin:asset ?org_value .
-    BIND(strbefore(?org_date,"T") as ?date)
+SELECT ?date (xsd:decimal(?org_asset) as ?asset) 
+(xsd:decimal(?org_liab) as ?liab) WHERE {
+<%URI%> l4a-fin:data ?financial_data.
+?financial_data l4a-fin:date ?org_date .
+?financial_data l4a-fin:asset ?org_asset.
+?financial_data l4a-fin:liab ?org_liab.
+BIND(strbefore(?org_date,"T") as ?date)
 } ORDER BY (?date)
     `
     return sparql_query;
